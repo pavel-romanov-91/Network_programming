b3f80e1552152d81ecebbd0615e078d1f93b501d
Павел Романов <pavel-romanov-91@mail.ru>
2023-06-06 16:43:34 +0300

    part_2

---
+++ part_2_Client/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace part_2_Client
+{
+    internal class Program
+    {
+        static string command = " ";
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Добро пожаловать в приложение \"Курс валют\".");
+                Console.WriteLine("Введите Start для подключения к серверу");
+                command = Console.ReadLine();
+                if (command == "Start")
+                {
+
+
+                    IPEndPoint serverPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+                    Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                    clientSocket.Connect(serverPoint);
+
+                    while (true)
+                    {
+                        if (clientSocket.Connected)
+                        {
+                            Console.WriteLine("Введите валюту для конвертации.");
+                            Console.WriteLine("Валюта вводится двумя словами через пробел из списка доступных валют." +
+                                " Первой указываем валюту которую хотим конвертировать.");
+                            Console.WriteLine("1. EUR");
+                            Console.WriteLine("2. USD");
+                            Console.WriteLine("3. GBP");
+                            string message = Console.ReadLine();
+                            if (message == "Exit")
+                            {
+                                clientSocket.Send(Encoding.UTF8.GetBytes("Exit"));
+                                clientSocket.Disconnect(false);
+                                clientSocket.Close();
+                                command = "Exit";
+                                break;
+                            }
+                            if (message != null && message != "")
+                            {
+                                if (message.Contains(' '))
+                                {
+                                    string[] values = message.Split(" ");
+                                    values[0] = values[0].Trim();
+                                    values[1] = values[1].Trim();
+                                    clientSocket.Send(Encoding.UTF8.GetBytes(values[0] + " " + values[1] + " "));
+                                    byte[] buffer = new byte[1024];
+                                    clientSocket.Receive(buffer);
+                                    int counter = 0;
+                                    foreach (byte b in buffer)
+                                    {
+                                        if (b != 0)
+                                        { counter++; }
+                                        else
+                                        { break; }
+                                    }
+                                    byte[] answer = new byte[counter];
+                                    counter = 0;
+                                    foreach (byte b in buffer)
+                                    {
+                                        if (b != 0)
+                                        { answer[counter] = b; counter++; }
+                                        else
+                                        { break; }
+                                    }
+                                    Console.WriteLine($"В {values[0]} содержится {Encoding.UTF8.GetString(answer)} {values[1]}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Программы не обнаружила пробела в запросе. Просим соблюдать инструкции.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Программа не смогла установить соединение.");
+                            clientSocket.Close();
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (command != "Exit")
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ part_2_Server/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_2_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                IPEndPoint serverPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+                Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                serverSocket.Bind(serverPoint);
+                serverSocket.Listen(1000);
+                serverSocket.BeginAccept(AcceptConnectionCallback, serverSocket);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            Console.ReadLine();
+        }
+        static void AcceptConnectionCallback(IAsyncResult result)
+        {
+            try
+            {
+
+                if (result.AsyncState != null)
+                {
+                    byte[] data = new byte[1024];
+                    Socket server = (Socket)result.AsyncState;
+                    Socket client = server.EndAccept(result);
+                    Console.WriteLine($"Произошло подключение: {client.RemoteEndPoint} в {DateTime.Now} ");
+                    client.BeginReceive(data, 0, data.Length, SocketFlags.None, ClientReceiveMessageCallback, new ClientMessage(client, data));
+                    server.BeginAccept(AcceptConnectionCallback, server);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+        static void ClientReceiveMessageCallback(IAsyncResult result)
+        {
+            if (result.AsyncState != null)
+            {
+                ClientMessage clientMessage = (ClientMessage)result.AsyncState;
+                Socket client = clientMessage.GetClient();
+                byte[] data = clientMessage.GetData();
+                client.EndReceive(result);
+                string message = Encoding.UTF8.GetString(data);
+                if (message.StartsWith("Exit"))
+                {
+                    Console.WriteLine($"Клиент разорвал соединение : {client.RemoteEndPoint} в {DateTime.Now} ");
+                    client.Close();
+                }
+                else
+                {
+
+                    string[] currencys = message.Split(' ');
+                    string answer = CurrencyList.GetValue(currencys[0], currencys[1]).ToString();
+                    byte[] senddata = Encoding.UTF8.GetBytes(answer);
+
+                    Console.WriteLine($"Клиент : {client.RemoteEndPoint} запросил перевод {message.Substring(0, 8)} в {DateTime.Now} ");
+                    client.BeginSend(senddata, 0, senddata.Length, SocketFlags.None, ClientSendMessageCallback, client);
+                    if (client != null)
+                    {
+                        client.BeginReceive(data, 0, data.Length, SocketFlags.None, ClientReceiveMessageCallback, new ClientMessage(client, data));
+                    }
+                }
+            }
+        }
+        static void ClientSendMessageCallback(IAsyncResult result)
+        {
+            if (result.AsyncState != null)
+            {
+                Socket client = (Socket)result.AsyncState;
+                client.EndSend(result);
+            }
+        }
+        class ClientMessage
+        {
+            Socket _client;
+            byte[] _message = new byte[1024];
+            public ClientMessage(Socket client, byte[] message)
+            {
+                _client = client;
+                _message = message;
+            }
+            public Socket GetClient()
+            {
+                return _client;
+            }
+            public byte[] GetData()
+            {
+                return _message;
+            }
+        }
+        static class CurrencyList
+        {
+            static double EUR = 82.3736;
+            static double USD = 77.2422;
+            static double GBP = 93.7720;
+            static public double GetValue(string first, string second)
+            {
+                double result = 0;
+                switch (first)
+                {
+                    case "EUR": if (second == "USD") { result = EURtoUSD(); } else { result = EURtoGBP(); } break;
+                    case "USD": if (second == "EUR") { result = USDtoEUR(); } else { result = USDtoGBP(); } break;
+                    case "GBP": if (second == "EUR") { result = GBPtoEUR(); } else { result = GBPtoUSD(); } break;
+                }
+                return result;
+            }
+            static double USDtoEUR() { return USD / EUR; }
+            static double USDtoGBP() { return USD / GBP; }
+            static double EURtoUSD() { return EUR / USD; }
+            static double EURtoGBP() { return EUR / GBP; }
+            static double GBPtoUSD() { return GBP / USD; }
+            static double GBPtoEUR() { return GBP / EUR; }
+
+        }
+    }
+}
